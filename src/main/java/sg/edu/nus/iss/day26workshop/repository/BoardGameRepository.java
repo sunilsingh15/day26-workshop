f5c000f3d184bcb9a1d3f9d06aa42b8bd9382052
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-07-09 13:51:45 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/day26workshop/repository/BoardGameRepository.java
@@ -0,0 +1,8 @@
+package sg.edu.nus.iss.day26workshop.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoardGameRepository {
+    
+}
---
+++ src/main/java/sg/edu/nus/iss/day26workshop/service/BoardGameService.java
@@ -0,0 +1,8 @@
+package sg.edu.nus.iss.day26workshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardGameService {
+
+}
